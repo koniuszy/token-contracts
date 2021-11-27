ca578e3e397213741195c57729ac4ec05b5772a6
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-11-27 23:34:29 +0100

    split contracts and build an architecture

---
+++ contracts/Token.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "./Ownable.sol";
+import "./ERC20.sol";
+
+/**
+ * @title Token
+ * @dev BEP20 compatible token.
+ */
+contract Token is ERC20Burnable, Ownable {
+    /**
+     * @dev Mints all tokens to deployer
+     * @param amount Initial supply
+     * @param name Token name.
+     * @param symbol Token symbol.
+     */
+    constructor(
+        uint256 amount,
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) {
+        _mint(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     *
+     * IMPORTANT: This method is required to be able to transfer tokens directly between their Binance Chain
+     * and Binance Smart Chain. More on this issue can be found in:
+     * https://github.com/binance-chain/BEPs/blob/master/BEP20.md#5116-getowner
+     */
+    function getOwner() external view returns (address) {
+        return owner();
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     */
+    function decimals() public pure override returns (uint8) {
+        return 4;
+    }
+}
